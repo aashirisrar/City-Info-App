e8c77150fb1bd7019d5bb08dae3e63d85663b68b
Aashir Israr <aashirisrar968@gmail.com>
2023-01-18 01:13:16 +0500

    made some changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname, "/public/index.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.get("/search", (req, res) => {
